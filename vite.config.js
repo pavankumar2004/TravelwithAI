b01d05f90fd5bb6154837f4c6f80025fd4b64a47
pavankumar2004 <pk6122004@gmail.com>
2025-01-26 00:57:38 +0530

    fix: config

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,17 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      // eslint-disable-next-line no-undef
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+  define: {
+    "process.env.VITE_GOOGLE_AUTH_CLIENT_ID": JSON.stringify(process.env.VITE_GOOGLE_AUTH_CLIENT_ID),
+    "process.env.VITE_GOOGLE_GEMINI_AI_API_KEY": JSON.stringify(process.env.VITE_GOOGLE_GEMINI_AI_API_KEY),
+    "process.env.VITE_GOOGLE_PLACES_API_KEY": JSON.stringify(process.env.VITE_GOOGLE_PLACES_API_KEY),
+  },
+});
